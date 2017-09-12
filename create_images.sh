1bf80b1950bcc4eae3636464d9ffb01d7bf4ccab
Olivier Pieters <me@olivierpieters.be>
2017-09-12 23:44:56 +0200

    Optimised image sizes.

--- create_images.sh
+++ create_images.sh
@@ -7,8 +7,8 @@ xelatex mwe-xelatex.tex
 pdf2svg mwe-xelatex.pdf cv%d.svg all
 mv cv1.svg ../images/cv.svg
 mv cv2.svg ../images/cover_letter.svg
-convert -density 1200 -border 10 -bordercolor black ../images/cv.svg ../images/cv.png
-convert -density 1200 -border 10 -bordercolor black ../images/cover_letter.svg ../images/cover_letter.png
+convert -density 100 -border 2 -bordercolor black ../images/cv.svg ../images/cv.png
+convert -density 100 -border 2 -bordercolor black ../images/cover_letter.svg ../images/cover_letter.png
 popd
 
  
