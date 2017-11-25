92db3a462ed4a95fc4a0f1cb3cf27799bab0f851
Olivier Pieters <me@olivierpieters.be>
2017-11-25 13:45:37 +0100

    Create CTAN tarball for upload.

---
+++ pack.sh
@@ -0,0 +1,88 @@
+#!/usr/bin/env bash
+
+echo "Copying files..."
+
+mkdir limecv
+mkdir limecv/examples/
+cp src/limecv.{dtx,ins} limecv/
+cp examples/*.tex limecv/examples
+cp examples/limecv-icon.pdf limecv/examples/
+cp examples/picture.png limecv/examples/
+cp README.md limecv/
+cp LICENSE limecv/
+
+echo "Building project..."
+pushd limecv/
+
+# create class file
+pdflatex limecv.ins
+
+cp limecv.cls examples/
+
+# build examples
+pushd examples/
+
+pdflatex mwe-latex.tex
+pdflatex mwe-latex.tex
+pdflatex mwe-latex.tex
+
+xelatex mwe-xelatex.tex
+xelatex mwe-xelatex.tex
+xelatex mwe-xelatex.tex
+
+lualatex mwe-lualatex.tex
+lualatex mwe-lualatex.tex
+lualatex mwe-lualatex.tex
+
+rm *.{aux,log,gz,cls,out}
+
+cp mwe-xelatex.pdf ../
+
+popd
+
+# build documentation
+pdflatex limecv.dtx
+pdflatex limecv.dtx
+pdflatex limecv.dtx
+
+echo "Cleaning project..."
+
+rm *.{aux,glo,log,out,toc,cls,idx}
+rm mwe-xelatex.pdf
+
+echo "Creating ZIP file"
+
+#ctanify limecv.ins \
+#  limecv.dtx \
+#  limecv.pdf \
+#  --no-skip README.md \
+#  --no-skip LICENSE \
+#  --no-skip examples/limecv-icon.pdf \
+#  --no-skip examples/picture.png \
+#  --no-skip examples/mwe-xelatex.tex \
+#  --no-skip examples/mwe-xelatex.pdf \
+#  --no-skip examples/mwe-latex.tex \
+#  --no-skip examples/mwe-latex.pdf \
+#  --no-skip examples/mwe-lualatex.tex \
+#  --no-skip examples/mwe-lualatex.pdf
+
+popd
+
+mkdir limecv/limecv/
+mv limecv/* limecv/limecv/
+
+COPYFILE_DISABLE=1 tar -pczf limecv.tar.gz limecv/
+
+echo "Cleaning..."
+
+rm -rf limecv
+
+echo "Contents:"
+
+tar -ztvf limecv.tar.gz
+
+echo "Done."
+
+
+
+
