6fb121096429902fe018bbd43a260a8f6c8896c0
Olivier Pieters <me@olivierpieters.be>
2018-01-17 20:44:07 +0100

    Fix another directory issue.

--- support/install_fira.sh
+++ support/install_fira.sh
@@ -1,6 +1,6 @@
 # source: https://gist.github.com/muammar/a5ffb635eb7f532346a8e777b847f8a7#file-getfirafonts-sh-L16
 
-cd /tmp
+pushd /tmp
 
 sudo apt-get install unzip -y
 wget "http://www.carrois.com/downloads/fira_4_1/FiraFonts4106.zip"
@@ -12,3 +12,5 @@ sudo mkdir -p /usr/share/fonts/opentype/FiraSans
 sudo cp Fira*/WEB/*.ttf /usr/share/fonts/truetype/FiraSans/
 sudo cp Fira*/OTF/Fira* /usr/share/fonts/opentype/FiraSans/
 sudo fc-cache -fv
+
+popd
