1479bceff8399402ad267cccfadb9aec4864aa4d
Olivier Pieters <me@olivierpieters.be>
2018-01-21 22:29:08 +0100

    Install fontawesome.

--- support/install_fira.sh
+++ support/install_fira.sh
@@ -4,17 +4,35 @@
 
 pushd /tmp
 
+# install unzip
 sudo apt-get install unzip -y
-sudo mkdir fonts
-pushd fonts
+# create directory for cache
+sudo mkdir -p limecvFonts
+pushd limecvFonts
+
+# download fonts
 wget "http://www.carrois.com/downloads/fira_4_1/FiraFonts4106.zip" -O FiraMainFonts.zip
 wget "http://www.carrois.com/downloads/fira_mono_3_2/FiraMonoFonts3206.zip"  -O FiraMonoFonts.zip
+wget "https://github.com/FortAwesome/Font-Awesome/archive/v4.7.0.zip" -O FontAwesome.zip
+
+# extract fonts
 unzip FiraMainFonts.zip -d FiraMain
 unzip FiraMonoFonts.zip -d FiraMono
+unzip FontAwesome.zip -d FontAwesome
+
+# create target directories for fonts
 sudo mkdir -p /usr/share/fonts/truetype/FiraSans
 sudo mkdir -p /usr/share/fonts/opentype/FiraSans
+sudo mkdir -p /usr/share/fonts/truetype/FontAwesome
+sudo mkdir -p /usr/share/fonts/opentype/FontAwesome
+
+# copy fonts
 sudo cp FiraMain/Fira*/WEB/TTF/*.ttf /usr/share/fonts/truetype/FiraSans/
 sudo cp FiraMono/Fira*/OTF/Fira* /usr/share/fonts/opentype/FiraSans/
+sudo cp FontAwesome/Font-Awesome*/fonts/fontawesome-webfont.ttf /usr/share/fonts/truetype/FontAwesome/fontawesome.ttf
+sudo cp FontAwesome/Font-Awesome*/fonts/FontAwesome.otf /usr/share/fonts/opentype/FontAwesome/
+
+# build cache
 sudo fc-cache -fv
 
 popd
