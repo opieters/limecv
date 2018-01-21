1cf4e8a44f43f008c79b8a2bbfca9a0209b8dfc6
Olivier Pieters <me@olivierpieters.be>
2018-01-21 22:05:15 +0100

    Fixed issues.

--- support/install_fira.sh
+++ support/install_fira.sh
@@ -1,16 +1,18 @@
-# source: https://gist.github.com/muammar/a5ffb635eb7f532346a8e777b847f8a7#file-getfirafonts-sh-L16
+#!/bin/bash
+
+# cf from http://programster.blogspot.com/2014/05/ubuntu-14-desktop-install-fira-sans-and.html
 
 pushd /tmp
 
 sudo apt-get install unzip -y
-wget "http://www.carrois.com/downloads/fira_4_1/FiraFonts4106.zip"
-wget "http://www.carrois.com/downloads/fira_mono_3_2/FiraMonoFonts3206.zip"
-unzip FiraSans4106.zip
-unzip FiraMono3206.zip
+wget "http://www.carrois.com/downloads/fira_4_1/FiraFonts4106.zip" -O FiraMainFonts.zip
+wget "http://www.carrois.com/downloads/fira_mono_3_2/FiraMonoFonts3206.zip"  -O FiraMonoFonts.zip
+unzip FiraMainFonts.zip -d FiraMain
+unzip FiraMonoFonts.zip -d FiraMono
 sudo mkdir -p /usr/share/fonts/truetype/FiraSans
 sudo mkdir -p /usr/share/fonts/opentype/FiraSans
-sudo cp Fira*/WEB/*.ttf /usr/share/fonts/truetype/FiraSans/
-sudo cp Fira*/OTF/Fira* /usr/share/fonts/opentype/FiraSans/
+sudo cp FiraMain/Fira*/WEB/TTF/*.ttf /usr/share/fonts/truetype/FiraSans/
+sudo cp FiraMono/Fira*/OTF/Fira* /usr/share/fonts/opentype/FiraSans/
 sudo fc-cache -fv
 
 popd
