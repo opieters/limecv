28135a58ad68f8eeb14f3571c630d8922bd35eb4
Olivier Pieters <me@olivierpieters.be>
2018-05-21 11:24:46 +0200

    Change fira download location.

--- support/install_fonts.sh
+++ support/install_fonts.sh
@@ -12,10 +12,7 @@ pushd limecvFonts
 
 # download fonts
 if [ ! -f FiraMainFonts.zip ]; then
-    wget "http://www.carrois.com/downloads/fira_4_1/FiraFonts4106.zip" -O FiraMainFonts.zip
-fi
-if [ ! -f FiraMonoFonts.zip ]; then
-    wget "http://www.carrois.com/downloads/fira_mono_3_2/FiraMonoFonts3206.zip"  -O FiraMonoFonts.zip
+    wget "https://github.com/mozilla/Fira/archive/4.202.zip" -O FiraMainFonts.zip
 fi
 if [ ! -f FontAwesome.zip ]; then
     wget "https://github.com/FortAwesome/Font-Awesome/archive/v4.7.0.zip" -O FontAwesome.zip
@@ -23,7 +20,6 @@ fi
 
 # extract fonts
 unzip FiraMainFonts.zip -d FiraMain
-unzip FiraMonoFonts.zip -d FiraMono
 unzip FontAwesome.zip -d FontAwesome
 
 # create target directories for fonts
@@ -33,8 +29,8 @@ sudo mkdir -p /usr/share/fonts/truetype/FontAwesome
 sudo mkdir -p /usr/share/fonts/opentype/FontAwesome
 
 # copy fonts
-sudo cp FiraMain/Fira*/WEB/TTF/*.ttf /usr/share/fonts/truetype/FiraSans/
-sudo cp FiraMono/Fira*/OTF/Fira* /usr/share/fonts/opentype/FiraSans/
+sudo cp FiraMain/Fira*/ttf/*.ttf /usr/share/fonts/truetype/FiraSans/
+sudo cp FiraMain/Fira*/otf/*.otf /usr/share/fonts/opentype/FiraSans/
 sudo cp FontAwesome/Font-Awesome*/fonts/fontawesome-webfont.ttf /usr/share/fonts/truetype/FontAwesome/fontawesome.ttf
 sudo cp FontAwesome/Font-Awesome*/fonts/FontAwesome.otf /usr/share/fonts/opentype/FontAwesome/
 
