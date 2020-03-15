6c4c47b489a7d723eaabbbe293089aae93fa6330
Olivier Pieters <me@olivierpieters.be>
2020-03-15 18:41:02 +0100

    Update install script to use Font Awesome 5.

--- support/install_fonts.sh
+++ support/install_fonts.sh
@@ -15,7 +15,7 @@ if [ ! -f FiraMainFonts.zip ]; then
     wget "https://github.com/mozilla/Fira/archive/4.202.zip" -O FiraMainFonts.zip
 fi
 if [ ! -f FontAwesome.zip ]; then
-    wget "https://github.com/FortAwesome/Font-Awesome/archive/v4.7.0.zip" -O FontAwesome.zip
+    wget "https://github.com/FortAwesome/Font-Awesome/releases/download/5.12.1/fontawesome-free-5.12.1-desktop.zip" -O FontAwesome.zip
 fi
 
 # extract fonts
@@ -31,8 +31,7 @@ sudo mkdir -p /usr/share/fonts/opentype/FontAwesome
 # copy fonts
 sudo cp FiraMain/Fira*/ttf/*.ttf /usr/share/fonts/truetype/FiraSans/
 sudo cp FiraMain/Fira*/otf/*.otf /usr/share/fonts/opentype/FiraSans/
-sudo cp FontAwesome/Font-Awesome*/fonts/fontawesome-webfont.ttf /usr/share/fonts/truetype/FontAwesome/fontawesome.ttf
-sudo cp FontAwesome/Font-Awesome*/fonts/FontAwesome.otf /usr/share/fonts/opentype/FontAwesome/
+sudo cp FontAwesome/otfs/*.otf /usr/share/fonts/opentype/FontAwesome/
 
 # build cache
 sudo fc-cache -fv
